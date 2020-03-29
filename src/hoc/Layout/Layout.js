642735858de845053496f3a3c1832f0f24438422
mohamedikram-vakil <mohamedikram.n@vakilsearch.com>
2020-03-29 21:43:39 +0530

    redux burger

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../Auxx/Aux';
+import Aux from '../Auxx/Auxx';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
